66d1efdeeba4e9218559f572772d2be1ed2ba629
jessigonzalez <jg5108@nyu.edu>
2019-10-03 15:18:50 -0400

    Update 1

---
+++ build/precache-manifest.08fc468112c94df9a330a33acbcbfc9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eeef3d3f1bbe64e09c078c56cfd4892",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b7fac787ab56e35202a",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d41ddb41bca3a416ef16",
+    "url": "/static/js/2.273c3a1e.chunk.js"
+  },
+  {
+    "revision": "8b7fac787ab56e35202a",
+    "url": "/static/js/main.e001fad3.chunk.js"
+  },
+  {
+    "revision": "7fa6085610bad4dcf8ed",
+    "url": "/static/js/runtime-main.18ba534d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
