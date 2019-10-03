c8cf9062c38e8c3a44f30b90f5a5158b6db93b82
jessigonzalez <jg5108@nyu.edu>
2019-10-03 16:04:13 -0400

    Src files

--- src/components/PageWrapper/index.js
+++ src/components/PageWrapper/index.js
@@ -5,6 +5,22 @@ export default function PageWrapper({cloudy,children}){
     const redValue = ((cloudy + 1)/100) *255;
     const blueValue = ((cloudy + 1)/125) *255;
     const greenValue = ((cloudy + 1)/ cloudy+1)*255;
+
+    const blueSky = `135,206,235`;
+    const cloudySky = `174,196,222`;
+    const night = `0,0,0`;
+    const currentDate = new Date();
+    const currentTime = currentDate.getHours();
+
+    let currentSky = blueSky;
+
+    if(cloudy>50){
+      currentSky = cloudySky
+    }
+    if(currentTime >19 || currentTime <5){
+      currentSky = night;
+    }
+    
   return(
     <div style={
       height: '100%';
