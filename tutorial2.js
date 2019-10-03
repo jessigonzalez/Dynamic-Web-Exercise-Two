74d724d8c46343cda5df7b0d60393f4586f0c907
jessigonzalez <jg5108@nyu.edu>
2019-10-03 15:41:47 -0400

    Into to APIs

---
+++ tutorial2.js
@@ -0,0 +1,19 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCloudRain, faCloud, faSun, faWind} from '@fortawesome-free-solid-svg-icons';//
+
+export default function WeatherIcon({weatherType}){
+  switch(weatherType){
+    case 'faCloudRain':
+      return ( <FontAwesomeIcon icon ={faCloudRain}/>
+      );
+    case 'cloudy':
+      return ( <FontAwesomeIcon icon ={faCloud} className="WeatherIcon" size="6x"/>
+    );
+    default:
+      return <div>{weatherType}</div>
+  }
+  return(
+    //<div>{weatherType}</div>
+    <FontAwesomeIcon icon ={faCloudRain};
+  )
+}
